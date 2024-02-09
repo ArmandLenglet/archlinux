d5d865231cdb87396faba0696fe25cafbafa4d69
ArmandLenglet <37247089+ArmandLenglet@users.noreply.github.com>
2024-02-09 16:29:19 +0100

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -8,16 +8,15 @@ timedatectl status
 mkfs.fat -F32 /dev/nvme1n1p1
 mkswap /dev/nvme1n1p2
 mkfs.ext4 /dev/nvme1n1p3
-
 swapon /dev/nvme1n1p2
+
+mkdir /mnt/boot
+mount /dev/nvme1n1p1 /mnt/boot
+mount /dev/nvme1n1p3 /mnt
 
 pacstrap /mnt base base-devel linux linux-firmware dhcpcd
 
 pacman -S archlinux-keyring
-
-mkdir -p /mnt/boot
-mount /dev/nvme1n1p1 /mnt/boot
-mount /dev/nvme1n1p3 /mnt
 
 genfstab -L /mnt >> /mnt/etc/fstab
 
