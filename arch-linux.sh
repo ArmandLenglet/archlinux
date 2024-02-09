c7228346b9f16b6f268dec7fdc64c7e70c1d1a13
ArmandLenglet <37247089+ArmandLenglet@users.noreply.github.com>
2024-02-09 15:31:12 +0100

    Update arch-linux.sh

--- arch-linux.sh
+++ arch-linux.sh
@@ -4,19 +4,19 @@ pacman -Syy
 timedatectl set-ntp true
 timedatectl status
 
-mkfs.fat -F32 /dev/nvme1p1
-mkswap /dev/nvme1p2
-mkfs.ext4 /dev/nvme1p3
+mkfs.fat -F32 /dev/nvme1n1p1
+mkswap /dev/nvme1n1p2
+mkfs.ext4 /dev/nvme1n1p3
 
-mount /dev/nvme1p3 /mnt
-swapon /dev/nvme1p2
+mount /dev/nvme1n1p3 /mnt
+swapon /dev/nvme1n1p2
 
 pacstrap /mnt base base-devel linux linux-headers linux-firmware
 
 pacman -Sy archlinux-keyring
 
-mkdir -p /mnt/boot/efi
-mount /dev/nvme1p1 /mnt/boot/efi
+mkdir -p /mnt/boot
+mount /dev/nvme1n1p1 /mnt/boot
 
 genfstab -U /mnt >> /mnt/etc/fstab
 
